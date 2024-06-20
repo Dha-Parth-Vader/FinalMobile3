507e4785d0245c2a2224690403293e39dfea9f71
Aaron Huang <144165021+ahuang683@users.noreply.github.com>
2024-06-20 15:05:52 -0700

    v

---
+++ app/src/main/java/com/example/myapplication/PDFConverter.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PDFConverter {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+        }
+
+
+}
