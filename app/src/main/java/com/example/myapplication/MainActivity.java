6ebc708ed2b8dfce48c773db8123aa6783b51f36
Parth Dhaulakhandi <128411192+Dha-Parth-Vader@users.noreply.github.com>
2024-06-20 13:55:36 -0700

    IT'S FIXED IT'S FINALLY FIXED
    I'VE FINALLY FIXED THE HOME SCREEN

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
